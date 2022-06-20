05e768082779d75cf5641cb78b13bb7077b627a6
Emily Ho <E176205@ameren.com>
2022-06-20 09:25:20 -0500

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,57 @@
+import styles from './About.module.css';
+import React from "react";
+// import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import MapContainer from './map.js';
+
 function About() {
-  return <div>About Page</div>;
+  return (
+    <div>
+        <h1 className={styles.heading}> About the Innovation Center </h1>
+        <div className={styles.statement}> 
+          <div> The Ameren Innovation Center collaborates with students 
+            and faculty from the University of Illinois and other 
+            companies across multiple industries at the Research Park. 
+            Their goal is to foster an exploratory environment that focuses
+            on innovations to help Ameren better meet the future needs of 
+            its customers. Opened in 2015, the Ameren Innovation Center uses a
+            multi-disciplinary workforce of interns and full-timers to 
+            leverage the kind of innovative technology needed for Ameren 
+            Digital to seize business opportunities and bring value to 
+            customers. </div>
+        </div>
+
+      <div class={styles.container}>
+        <div className={styles.box}>
+          <h2> Founded in 2015 </h2>
+        </div>
+        <div className={styles.box}>
+          <h2> 14 Interns </h2>
+        </div>
+        <div className={styles.box}>
+          <h2> Research Park UIUC </h2>
+        </div>
+      </div>
+
+      {/* <div class={styles.containertwo}>
+        <div className={styles.box}>
+          <h3> Our Mission</h3>
+            <p> Mission statement</p>
+        </div>
+        <div className={styles.box}>
+          <h3> Our Goals</h3>
+            <p> Our goals</p>
+        </div>
+      </div> */}
+
+      <div className={styles.contact}>
+        <h2> Contact us to get in touch! </h2>
+        <div> 2100 S. First St. Champaign, IL</div>
+        <div> Kevin Ficek: KFicek@ameren.com </div>
+        <div> James Kabbes: JKabbes@ameren.com </div>
+      </div>
+
+    </div>
+  );
 }
 
 export default About;
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,5 +1,37 @@
+import styles from './People.module.css';
+// import archit from './images/data/archit.jpeg'
+// import david from './images/data/david-an.jpeg'
+
+
 function People() {
-  return <div>People</div>;
+  return  (
+    <div> 
+      <h1 className={styles.title}>Teams</h1>
+        <h2 className={styles.team}>Application Development</h2>
+          <ul className={styles.list}>
+            <li>Edward Tomlins</li>
+            <li>Emily Ho</li>
+            <li>Jacom Chang</li>
+            <li>William Tegge</li>
+            <li>Zakaria-Ramdan Sadoun</li>
+          </ul>
+        <h2 className={styles.team}>Data Science</h2>
+          <ul className={styles.list}>
+            <li>Archit Myadam</li>
+            <li>David An</li>
+            <li>Jash Parekh</li>
+            <li>Myles Ream</li>
+            <li>Xander Pero</li>
+          </ul>
+        <h2 className={styles.team}>Innovation Operations</h2>
+          <ul className={styles.list}>
+            <li>George Atkins</li>
+            <li>Jack Aubry</li>
+            <li>Katie Kabbes</li>
+            <li>Ritika Patel</li>
+          </ul>
+    </div>
+  );
 }
 
 export default People;
