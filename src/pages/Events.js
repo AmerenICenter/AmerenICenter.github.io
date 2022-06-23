fb33d9456ca566692201a1b39a84110ac5ad6239
Emily Ho <E176205@ameren.com>
2022-06-23 08:16:22 -0500

    added information and photos to event page timeline

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -3,72 +3,72 @@ import styles from './Events.module.css';
 
 const items = [
   {
-    title: 'Feb 2015',
-    cardTitle: 'Ameren Innovation Center Opens',
+    title: 'June 1st, 2022',
+    cardTitle: 'Ameren iCenter Project Kickoff',
     url: 'http://www.history.com',
-    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardSubtitle: 'The start of summer work!',
     cardDetailedText:
-      'Men of the British Expeditionary Force (BEF) wade out to..',
+      'Project sponsors present to iCenter interns their vision for each project. Electric Vehicle Analysis, Gas Usage Forecasting, JD Power, Renewables Tracker, iCenter Website, and Smart Documentation were all introduced to the team. Each intern started work creating a quality product for the sponsors.  ',
     media: {
       type: 'IMAGE',
       source: {
-        url: 'http://someurl/image.jpg',
+        url: '../images/events/iCenter.jpg',
       },
     },
   },
   {
-    title: 'May 1940',
-    cardTitle: 'Dunkirk',
+    title: 'June 2nd, 2022',
+    cardTitle: '2nd Annual Hackathon – Snake Game',
     url: 'http://www.history.com',
-    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardSubtitle: "The App Development and Data Team participated in the Ameren iCenter's 2nd Annual Hackathon!",
     cardDetailedText:
-      'Men of the British Expeditionary Force (BEF) wade out to..',
+      "The App Development and Data Team participated in the Ameren iCenter's 2nd Annual Hackathon! The team worked together to recreate the classic game 'Snake' in Python. Within 8 hours they were able to finish the game, create a website hosting Snake, and finally present everything to management. Several applications were used, including Anaconda, GitHub, and the PyGame library. The iOps team supported the game development by creating deadlines, delegating tasks, and documenting the whole experience.",
     media: {
       type: 'IMAGE',
       source: {
-        url: 'http://someurl/image.jpg',
+        url: './images/events/snake.png',                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
       },
     },
   },
   {
-    title: 'May 1940',
-    cardTitle: 'Dunkirk',
+    title: 'June 6th, 2022',
+    cardTitle: 'First Softball Win!',
     url: 'http://www.history.com',
-    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardSubtitle: 'The Ameren Transformers hit the field to start their softball season!',
     cardDetailedText:
-      'Men of the British Expeditionary Force (BEF) wade out to..',
+      'Team manager Kevin pitched a shutout and led the team to a commanding victory. They won the game 38-0 and began the season on the right foot.',
     media: {
       type: 'IMAGE',
       source: {
-        url: 'http://someurl/image.jpg',
+        url: './images/events/softball.png',
       },
     },
   },
   {
-    title: 'May 1940',
-    cardTitle: 'Dunkirk',
+    title: 'June 8th, 2022',
+    cardTitle: 'Data Science Hackathon',
     url: 'http://www.history.com',
-    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardSubtitle: 'Watch out, it’s Hackathon time!',
     cardDetailedText:
-      'Men of the British Expeditionary Force (BEF) wade out to..',
+      'The Data team participated in their second Hackathon of the summer. This time they focused their efforts on data exploration and visualization. They were given a large data set, around 12 million rows, containing an entire year of electric meter data for southern Illinois. In six hours, they produced over ten deliverables including analyzing 24-hour usage patterns, average customer usage, and creating a model to predict the next day usage based on the temperature forecast. The interns received a talk during lunch from an experienced data scientist of over 20 years. At the end of the day, the team presented these deliverables to upper-level management.',
     media: {
       type: 'IMAGE',
       source: {
-        url: 'http://someurl/image.jpg',
+        url: './images/events/datahackathon.PNG',
       },
     },
   },
   {
-    title: 'May 1940',
-    cardTitle: 'Dunkirk',
+    title: 'June 9th, 2022',
+    cardTitle: 'TAC Tour',
     url: 'http://www.history.com',
-    cardSubtitle: 'Men of the British Expeditionary Force (BEF) wade out to..',
+    cardSubtitle: 'Touring the Technology Application Center (TAC)',
     cardDetailedText:
-      'Men of the British Expeditionary Force (BEF) wade out to..',
+    "It's time for a tour! The Ameren iCenter was fortunate enough to receive a tour of the Technology Application Center or TAC for short. This Ameren facility is responsible for offering on-grid testing services of new products, services or business models. They operate on a Smart Grid and can generate power through various methods, such as wind, solar, and gas. The TAC is the only place in the nation that can seamlessly transition an entire circuit, of around 200 homes, from the traditional power grid to their personal Smart Grid. Thanks to the full-time TAC employees, we were able to get up close and personal with all the cutting-edge technology.",
     media: {
       type: 'IMAGE',
       source: {
-        url: 'http://someurl/image.jpg',
+        url: './images/events/tac-tour(2).png',
       },
     },
   },
