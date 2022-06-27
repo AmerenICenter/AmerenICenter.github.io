76aa47de0aae7fd25a51ac038144bf7b2d619030
wtegge2 <80014792+wtegge2@users.noreply.github.com>
2022-06-27 13:31:35 -0500

    Put in Kevin's info

--- src/data/team.js
+++ src/data/team.js
@@ -1,6 +1,6 @@
 export const managerTeam = [
   {
-    name: 'Jacob Chang',
+    name: 'Joe Rolfes',
     major: 'Computer Science',
     graduationDate: 'May 2025',
     links: [
@@ -16,36 +16,36 @@ export const managerTeam = [
   },
   {
     name: 'James Kabbes',
-    major: 'Computer Science',
-    graduationDate: 'May 2025',
+    major: 'Mechanical Engineering',
+    graduationDate: 'May 2020',
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
       { LinkedIn: 'https://www.linkedin.com' },
     ],
-    headshot: '../../images/headshots/data/james.jfif',
+    headshot: '../../images/headshots/data/james.png',
     biography:
-      "Professional Dream - I have aspirations to become an executive. I want to lead high-functioning teams. Claim to Fame - I hold the record for most wins in University of Illinois intramural athletics.",
+      'Professional Dream - I have aspirations to become an executive. I want to lead high-functioning teams. Claim to Fame - I hold the record for most wins in University of Illinois intramural athletics.',
     skills: ['A', 'B', 'C'],
     team: 'Data Scientist',
   },
   {
-    name: 'Jacob Chang',
-    major: 'Computer Science',
-    graduationDate: 'May 2025',
+    name: 'Kevin Ficek',
+    major: 'Mechanical Engineering',
+    graduationDate: 'December 1996',
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { LinkedIn: 'https://www.linkedin.com/in/kevin-ficek-babb94215/' },
     ],
-    headshot: '../../images/headshots/app/jacobchang.jpg',
+    headshot: '../../images/headshots/app/kevin.jpg',
     biography:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+      "Favorite Star Wars Movie: Rogue One. Least Favorite Star Wars Movie: Solo",
     skills: ['A', 'B', 'C'],
     team: 'Head Manager',
   },
 ];
- 
+
 export const appTeam = [
   {
     name: 'Jacob Chang',
@@ -58,7 +58,7 @@ export const appTeam = [
     ],
     headshot: '../../images/headshots/app/jacobchang.jpg',
     biography:
-      "",
+      "One fun fact about me is I used to speed solve Rubik's Cubes. My fastest official time as 12.47 seconds! Other hobbies I have include PC games, eating at restaurants, and listening to a variety of music.",
     skills: ['HTML', 'React', 'JavaScript'],
     team: 'Application Development',
   },
@@ -73,7 +73,7 @@ export const appTeam = [
     ],
     headshot: '../../images/headshots/app/emilyho.jpg',
     biography:
-      "What inspires me: Something that inspires me in my career is that there is always something new to learn and explore. What makes my day better: Some things that make my day better is my family, dogs, sunny days, and playing badminton!",
+      'What inspires me: Something that inspires me in my career is that there is always something new to learn and explore. What makes my day better: Some things that make my day better is my family, dogs, sunny days, and playing badminton!',
     skills: ['C++', 'Python', 'Kotlin'],
     team: 'Application Development',
   },
@@ -83,12 +83,12 @@ export const appTeam = [
     graduationDate: 'May 2024',
     links: [
       { YouTube: 'https://www.youtube.com' },
-      { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { Google: 'https://github.com/wtegge2' },
+      { LinkedIn: 'https://www.linkedin.com/in/williamtegge/' },
     ],
     headshot: '../../images/headshots/app/williamtegge.jpg',
     biography:
-      "Who or what inspires me in my career: My friends and family inspire me to push past my limits and work harder everyday. One thing on my bucket list: go skydiving.",
+      'Who or what inspires me in my career: My friends and family inspire me to push past my limits and work harder everyday. One thing on my bucket list: go skydiving.',
     skills: ['C/C++', 'Python', 'Github'],
     team: 'Application Development',
   },
@@ -97,13 +97,13 @@ export const appTeam = [
     major: 'Computer Engineering',
     graduationDate: 'May 2024',
     links: [
-      { YouTube: 'https://www.youtube.com' },
-      { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { YouTube: 'https://www.youtube.com/channel/UCPmK7bHWFmSEsWBwDim-2KQ' },
+      { Google: 'https://github.com/edtomlins' },
+      { LinkedIn: 'https://www.linkedin.com/in/edward-tomlins-69716b153/' },
     ],
     headshot: '../../images/headshots/app/edwardtomlins.jpg',
     biography:
-      "My professional dream is to work for a Biotech company designing products to help save lives and improve quality of life. Specifically, I would want to focus on hearts and the cardiovascular system of the body. One thing on my bucket list is to visit all of the National Parks in North America. I have been to several like Yellowstone and Grand Teton, but there are so many more that I would like to see and hike around!",
+      'My professional dream is to work for a Biotech company designing products to help save lives and improve quality of life. Specifically, I would want to focus on hearts and the cardiovascular system of the body. One thing on my bucket list is to visit all of the National Parks in North America. I have been to several like Yellowstone and Grand Teton, but there are so many more that I would like to see and hike around!',
     skills: ['C++', 'System Verilog', 'Graphic Design'],
     team: 'Application Development',
   },
@@ -113,17 +113,17 @@ export const appTeam = [
     graduationDate: 'May 2023',
     links: [
       { YouTube: 'https://www.youtube.com' },
-      { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { Google: 'https://github.com/zakosadoun' },
+      { LinkedIn: 'linkedin.com/in/zakaria-sadoun' },
     ],
     headshot: '../../images/headshots/app/zakariaramdansadoun.png',
     biography:
-      "My one year goal: My one year goal is to work as a full time software engineer. One thing on my bucket list: Owning a horse is on my bucket list.",
+      'My one year goal: My one year goal is to work as a full time software engineer. One thing on my bucket list: Owning a horse is on my bucket list.',
     skills: ['Java', 'CSS', 'HTML'],
     team: 'Application Development',
   },
 ];
- 
+
 export const dataTeam = [
   {
     name: 'David An',
@@ -136,7 +136,7 @@ export const dataTeam = [
     ],
     headshot: '../../images/headshots/data/davidan.jpg',
     biography:
-      "What is your professional dream? My professional dream is to be able to be in a position that allows me to help others grow both professionally and personally. I also want to let the people around me smile through my actions. Hobbies: I enjoy anything nature related. In my downtime, I enjoy reading, running, programming, and riding my bike. I hopefully aim to compete in various races and tours soon! ",
+      'What is your professional dream? My professional dream is to be able to be in a position that allows me to help others grow both professionally and personally. I also want to let the people around me smile through my actions. Hobbies: I enjoy anything nature related. In my downtime, I enjoy reading, running, programming, and riding my bike. I hopefully aim to compete in various races and tours soon! ',
     skills: ['Python', 'C++', 'Java'],
     team: 'Data Science',
   },
@@ -151,7 +151,7 @@ export const dataTeam = [
     ],
     headshot: '../../images/headshots/data/mylesream.jpg',
     biography:
-      "My big hairy audacious goal is to get a black belt in Brazilian Jiu Jitsu. Hobbies I have include martial arts, working out, tennis, cooking, music and video games.",
+      'My big hairy audacious goal is to get a black belt in Brazilian Jiu Jitsu. Hobbies I have include martial arts, working out, tennis, cooking, music and video games.',
     skills: ['A', 'B', 'C'],
     team: 'Data Science',
   },
@@ -181,7 +181,7 @@ export const dataTeam = [
     ],
     headshot: '../../images/headshots/data/architmyadam.jpeg',
     biography:
-      "My biggest strength is my speed in learning things, I am able to pickup new concepts very fast. Some hobbies I have is playing basketball and soccer with my friends.",
+      'My biggest strength is my speed in learning things, I am able to pickup new concepts very fast. Some hobbies I have is playing basketball and soccer with my friends.',
     skills: ['Java', 'Python', 'SQL'],
     team: 'Data Science',
   },
@@ -210,11 +210,10 @@ export const iopsTeam = [
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { LinkedIn: 'https://www.linkedin.com/in/kaitlyn-kabbes-302248208/' },
     ],
     headshot: '../../images/headshots/iops/katiekabbes.jpg',
-    biography:
-      "Fun Fact: My favorite intramural on campus is dodgeball.",
+    biography: 'Fun Fact: My favorite intramural on campus is dodgeball.',
     skills: ['Scrum Master', 'JIRA', 'Excel'],
     team: 'Innovations Operations',
   },
@@ -225,26 +224,26 @@ export const iopsTeam = [
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { LinkedIn: 'https://www.linkedin.com/in/george-atkins-04218621b' },
     ],
     headshot: '../../images/headshots/iops/georgeatkins.jpg',
     biography:
-      "My big hairy audacious goal is to summit the two tallest mountains in the world, Mount Everest and K2. I believe that everyone should go on an extended backpacking trip to get away from technology and embrace nature.",
+      'My big hairy audacious goal is to summit the two tallest mountains in the world, Mount Everest and K2. I believe that everyone should go on an extended backpacking trip to get away from technology and embrace nature.',
     skills: ['MS Office', 'Spreadsheets', 'Writing'],
     team: 'Innovations Operations',
   },
   {
-    name: 'ritika Patel',
+    name: 'Ritika Patel',
     major: 'Information Systems and Finance',
     graduationDate: 'May 2025',
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { LinkedIn: 'linkedin.com/in/rdp25' },
     ],
     headshot: '../../images/headshots/iops/ritikapatel.jpg',
     biography:
-      "One accomplishment I am proud of: Helping to create a website that talks about child abuse and how people can take action against it. The luckiest thing that has happened to me: Guessing the exact number of cheeseballs in a container to win a 100 dollar gift card",
+      'One accomplishment I am proud of: Helping to create a website that talks about child abuse and how people can take action against it. The luckiest thing that has happened to me: Guessing the exact number of cheeseballs in a container to win a 100 dollar gift card',
     skills: ['Java', 'C++', 'C'],
     team: 'Innovations Operations',
   },
@@ -255,7 +254,7 @@ export const iopsTeam = [
     links: [
       { YouTube: 'https://www.youtube.com' },
       { Google: 'https://www.google.com' },
-      { LinkedIn: 'https://www.linkedin.com' },
+      { LinkedIn: 'https://www.linkedin.com/in/jack-aubry-237b26206' },
     ],
     headshot: '../../images/headshots/iops/jackaubry.jpg',
     biography:
