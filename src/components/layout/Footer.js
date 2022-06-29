625995c9cb8c827514ea3100fb654684c899d524
Jacob <45279415+jacobc2700@users.noreply.github.com>
2022-06-29 15:11:49 -0400

    added tooltips from mui

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,10 +5,18 @@ import {
   faFacebook,
   faTwitterSquare,
 } from '@fortawesome/free-brands-svg-icons';
+import Tooltip from '@mui/material/Tooltip';
+import withStyles from '@mui/styles/withStyles';
 
 function getCurrentYear() {
   return new Date().getFullYear();
 }
+
+const StyledTooltip = withStyles({
+  tooltipPlacementTop: {
+    top: '-20px',
+  },
+})(Tooltip);
 
 function Footer() {
   return (
@@ -16,19 +24,42 @@ function Footer() {
       <div>
         <ul className={styles.links}>
           <li>
-            <a target='_blank' href='https://github.com/'>
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
+            <StyledTooltip
+              title={<span className={styles.tooltipText}>GitHub</span>}
+              placement='top'
+              arrow
+            >
+              <a target='_blank' rel='noreferrer' href='https://github.com/'>
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+            </StyledTooltip>
+          </li>
+
+          <li>
+            <StyledTooltip
+              title={<span className={styles.tooltipText}>Twitter</span>}
+              placement='top'
+              arrow
+            >
+              <a target='_blank' rel='noreferrer' href='https://twitter.com/'>
+                <FontAwesomeIcon icon={faTwitterSquare} />
+              </a>
+            </StyledTooltip>
           </li>
           <li>
-            <a target='_blank' href='https://twitter.com/'>
-              <FontAwesomeIcon icon={faTwitterSquare} />
-            </a>
-          </li>
-          <li>
-            <a target='_blank' href='https://www.facebook.com/'>
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
+            <StyledTooltip
+              title={<span className={styles.tooltipText}>Facebook</span>}
+              placement='top'
+              arrow
+            >
+              <a
+                target='_blank'
+                rel='noreferrer'
+                href='https://www.facebook.com/'
+              >
+                <FontAwesomeIcon icon={faFacebook} />
+              </a>
+            </StyledTooltip>
           </li>
         </ul>
       </div>
